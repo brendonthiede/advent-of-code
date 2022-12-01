8bbb0fc0e76b1b69b072a80145aebd84fc503f8c
Brendon Thiede <brendon.thiede@gmail.com>
2022-12-01 09:24:03 -0500

    Day 1 solution

---
+++ day1/solution.js
@@ -0,0 +1,19 @@
+fs = require('fs');
+const inputs = fs.readFileSync(`${__dirname}/input.txt`, 'utf8').split("\n").map(Number);
+
+const allElves = []
+let currentCalories = 0
+for (i = 0; i <= inputs.length; i++) {
+    if (i == inputs.length || inputs[i] == 0) {
+        allElves.push(currentCalories)
+        currentCalories = 0
+    } else {
+        currentCalories += inputs[i]
+    }
+}
+
+allElves.sort((a, b) => b - a )
+console.log("Answer for part 1: " + allElves[0])
+
+const topCalories = allElves.sort((a, b) => b - a ).slice(0, 3).reduce((a, b) => a + b, 0)
+console.log("Answer for part 2: " + topCalories)
