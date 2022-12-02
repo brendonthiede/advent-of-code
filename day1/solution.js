e72b627eb4b8d0e8ff37e4613caa893e4f4cdc82
Brendon Thiede <brendon.thiede@gmail.com>
2022-12-02 13:49:24 -0500

    Day 1 refactor

--- day1/solution.js
+++ day1/solution.js
@@ -1,19 +1,8 @@
 fs = require('fs');
-const inputs = fs.readFileSync(`${__dirname}/input.txt`, 'utf8').split("\n").map(Number);
+const inputs = fs.readFileSync(`${__dirname}/input.txt`, 'utf8')
+    .split(/\r?\n\r?\n/)
+    .map(val => val.split(/\r?\n/).map(Number).reduce((a, b) => a + b, 0))
+    .sort((a, b) => b - a);
 
-const allElves = []
-let currentCalories = 0
-for (i = 0; i <= inputs.length; i++) {
-    if (i == inputs.length || inputs[i] == 0) {
-        allElves.push(currentCalories)
-        currentCalories = 0
-    } else {
-        currentCalories += inputs[i]
-    }
-}
-
-allElves.sort((a, b) => b - a )
-console.log("Answer for part 1: " + allElves[0])
-
-const topCalories = allElves.slice(0, 3).reduce((a, b) => a + b, 0)
-console.log("Answer for part 2: " + topCalories)
+console.log("Answer for part 1: " + inputs[0])
+console.log("Answer for part 2: " + inputs.slice(0, 3).reduce((a, b) => a + b, 0))
