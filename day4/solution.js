cf5796e1c25c8b11dbf1f68f2744b2710a8c4472
Brendon Thiede <brendon.thiede@gmail.com>
2022-12-04 00:58:51 -0500

    Refactoring to use number prototype

--- day4/solution.js
+++ day4/solution.js
@@ -2,31 +2,36 @@ fs = require('fs');
 
 const inputs = fs.readFileSync(`${__dirname}/input.txt`, 'utf8').split(/\r?\n/)
 
-console.log("Answer for part 1: " + inputs.reduce((a, b) => {
-    const elves = b.split(',')
-    elfOneParts = elves[0].split('-').map(Number)
-    elfTwoParts = elves[1].split('-').map(Number)
+Number.prototype.isInRange = function (range) {
+    return this >= range[0] && this <= range[1]
+}
 
-    if ((elfOneParts[0] <= elfTwoParts[0] && elfOneParts[1] >= elfTwoParts[1])
-        || (elfOneParts[0] >= elfTwoParts[0] && elfOneParts[1] <= elfTwoParts[1])) {
-        return a + 1
+console.log("Answer for part 1: " + inputs.reduce((cumulative, line) => {
+    const elves = line.split(',')
+    elfOneTaskRange = elves[0].split('-').map(Number)
+    elfTwoTaskRange = elves[1].split('-').map(Number)
+
+    if ((elfOneTaskRange[0].isInRange(elfTwoTaskRange)
+        && elfOneTaskRange[1].isInRange(elfTwoTaskRange))
+        || (elfTwoTaskRange[0].isInRange(elfOneTaskRange)
+            && elfTwoTaskRange[1].isInRange(elfOneTaskRange))) {
+        return cumulative + 1
     } else {
-        return a
+        return cumulative
     }
 }, 0))
 
-// not 825
-console.log("Answer for part 2: " + inputs.reduce((a, b) => {
-    const elves = b.split(',')
-    elfOneParts = elves[0].split('-').map(Number)
-    elfTwoParts = elves[1].split('-').map(Number)
-
-    if ((elfOneParts[0] >= elfTwoParts[0] && elfOneParts[0] <= elfTwoParts[1])
-        || (elfOneParts[1] >= elfTwoParts[0] && elfOneParts[1] <= elfTwoParts[1])
-        || (elfTwoParts[0] >= elfOneParts[0] && elfTwoParts[0] <= elfOneParts[1])
-        || (elfTwoParts[1] >= elfOneParts[0] && elfTwoParts[1] <= elfOneParts[1])) {
-        return a + 1
+console.log("Answer for part 2: " + inputs.reduce((cumulative, line) => {
+    const elves = line.split(',')
+    elfOneTaskRange = elves[0].split('-').map(Number)
+    elfTwoTaskRange = elves[1].split('-').map(Number)
+    elfOneTaskRange[0]
+    if (elfOneTaskRange[0].isInRange(elfTwoTaskRange)
+        || elfOneTaskRange[1].isInRange(elfTwoTaskRange)
+        || elfTwoTaskRange[0].isInRange(elfOneTaskRange)
+        || elfTwoTaskRange[1].isInRange(elfOneTaskRange)) {
+        return cumulative + 1
     } else {
-        return a
+        return cumulative
     }
 }, 0))
