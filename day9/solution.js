ce0395da0a1cb9ac6a3eadeab24c521251c20163
Brendon Thiede <brendonthiede@filevine.com>
2022-12-09 19:02:16 -0500

    Day 9 solution

--- day9/solution.js
+++ day9/solution.js
@@ -10,52 +10,41 @@ movements = fs.readFileSync(`${__dirname}/input.txt`, 'utf8')
         }
     })
 
-const headPosition = { x: 0, y: 0 }
-const tailPosition = { x: 0, y: 0 }
-const visited = new Set()
-visited.add('0,0')
-
-function printDebug(header) {
-    console.log(header)
-    for (let row = 4; row >= 0; row--) {
-        let output = ''
-        for (let col = 0; col <= 5; col++) {
-            const pos = `${col},${row}`
-            if (pos === `${headPosition.x},${headPosition.y}`) {
-                output += 'H'
-            } else if (pos === `${tailPosition.x},${tailPosition.y}`) {
-                output += 'T'
-            } else if (visited.has(pos)) {
-                output += '#'
-            } else {
-                output += '.'
-            }
-        }
-        console.log(output)
+function moveTail(previousTail, currentTail) {
+    if (Math.abs(previousTail.x - currentTail.x) > 1 && Math.abs(previousTail.y - currentTail.y) > 1) {
+        currentTail.x += previousTail.x > currentTail.x ? 1 : -1
+        currentTail.y += previousTail.y > currentTail.y ? 1 : -1
+    } else if (Math.abs(previousTail.x - currentTail.x) > 1) {
+        currentTail.x += previousTail.x > currentTail.x ? 1 : -1
+        currentTail.y = previousTail.y
+    } else if (Math.abs(previousTail.y - currentTail.y) > 1) {
+        currentTail.y += previousTail.y > currentTail.y ? 1 : -1
+        currentTail.x = previousTail.x
     }
 }
 
-movements.forEach(movement => {
-    const { direction, distance } = movement
-    const { x, y } = headPosition
-    const { x: tx, y: ty } = tailPosition
-    const dx = direction === 'R' ? 1 : direction === 'L' ? -1 : 0
-    const dy = direction === 'U' ? 1 : direction === 'D' ? -1 : 0
-    for (let i = 0; i < distance; i++) {
-        headPosition.x += dx
-        headPosition.y += dy
+function getVisited(tailLength) {
+    const visited = new Set()
+    visited.add('0,0')
+    const positions = Array(tailLength + 1).fill(0).map(() => { return { x: 0, y: 0 } })
+    
+    movements.forEach(movement => {
+        const { direction, distance } = movement
+        const dx = direction === 'R' ? 1 : direction === 'L' ? -1 : 0
+        const dy = direction === 'U' ? 1 : direction === 'D' ? -1 : 0
+        for (let i = 0; i < distance; i++) {
+            positions[0].x += dx
+            positions[0].y += dy
 
-        if (Math.abs(headPosition.x - tailPosition.x) > 1) {
-            tailPosition.x += dx
-            tailPosition.y = headPosition.y
+            for (let index = 1; index < positions.length; index++) {
+                moveTail(positions[index - 1], positions[index])
+            }
+            visited.add(`${positions[positions.length - 1].x},${positions[positions.length - 1].y}`)
         }
-        if (Math.abs(headPosition.y - tailPosition.y) > 1) {
-            tailPosition.y += dy
-            tailPosition.x = headPosition.x
-        }
-        visited.add(`${tailPosition.x},${tailPosition.y}`)
-        // printDebug(`== ${direction} ${distance} ==`)
-    }
-})
+    })
 
-console.log(`Answer for part 1: ${visited.size}`)
+    return visited.size
+}
+
+console.log(`Answer for part 1: ${getVisited(1)}`)
+console.log(`Answer for part 2: ${getVisited(9)}`)
