e5524d6f60b1a53fef543a99f43405507a0ac39e
Brendon Thiede <brendonthiede@filevine.com>
2022-12-13 11:24:30 -0500

    Day 13 solution

--- day13/solution.js
+++ day13/solution.js
@@ -1,12 +1,8 @@
 fs = require('fs')
-const pairs = fs.readFileSync(`${__dirname}/input.txt`, 'utf8')
-    .replace(/\[/g, '[,')
-    .replace(/]/g, ',]')
-    .replace(/,,/g, ',')
-    .split(/\r?\n\r?\n/)
-    .map(x => x.split(/\r?\n/))
 
-let orderedPairs = []
+function splitPacket(packet) {
+    return packet.replace(/\[/g, '[,').replace(/]/g, ',]').replace(/,,/g, ',').split(',')
+}
 
 function isSorted(a, b) {
     let lhs = a.slice()
@@ -30,53 +26,31 @@ function isSorted(a, b) {
             }
         }
 
-        // === start of list with mixed types ===
-        if (lhsVal === '[' && !isNaN(rhsVal)) {
+        // === end of list ===
+        if (lhsVal === ']') {
+            // lhs is shorter
+            inOrder = true
+            lhs = []
+            break
+        } else if (rhsVal === ']') {
+            // rhs is shorter
+            inOrder = false
+            break
+        }
+
+        // === mixed types ===
+        if (lhsVal === '[') {
             rhs.splice(0, 0, ']')
             rhs.splice(0, 0, rhsVal)
             continue
-        }
-
-        if (!isNaN(lhsVal) && rhsVal === '[') {
+        } else {
             lhs.splice(0, 0, ']')
             lhs.splice(0, 0, lhsVal)
             continue
         }
-
-        // === end of list ===
-        // lhs is shorter
-        if (lhsVal === ']' && rhsVal !== ']') {
-            inOrder = true
-            lhs = []
-            break
-        }
-
-        // rhs is shorter
-        else if (lhsVal !== ']' && rhsVal === ']') {
-            inOrder = false
-            break
-        }
     }
 
     return inOrder && lhs.length <= rhs.length ? -1 : 1
-}
-
-pairs.forEach((pair, index) => {
-    let lhs = pair[0].split(',').map(val => isNaN(val) ? val : parseInt(val))
-    let rhs = pair[1].split(',').map(val => isNaN(val) ? val : parseInt(val))
-
-    if (isSorted(lhs, rhs) < 0) orderedPairs.push(index + 1)
-})
-
-console.log(`Answer for part 1 should be 5760`)
-console.log(`Answer for part 1: ${orderedPairs.reduce((a, b) => a + b, 0)}`)
-
-function splitPacket(packet) {
-    return packet
-        .replace(/\[/g, '[,')
-        .replace(/]/g, ',]')
-        .replace(/,,/g, ',')
-        .split(',')
 }
 
 const allPackets = fs.readFileSync(`${__dirname}/input.txt`, 'utf8')
@@ -84,13 +58,19 @@ const allPackets = fs.readFileSync(`${__dirname}/input.txt`, 'utf8')
     .split(/\r?\n/)
     .map(splitPacket)
 
+const orderedPairs = []
+for (let index = 0; index < allPackets.length / 2; index++) {
+    if (isSorted(allPackets[index * 2], allPackets[index * 2 + 1]) < 0) orderedPairs.push(index + 1)
+}
+
+console.log(`Answer for part 1: ${orderedPairs.reduce((a, b) => a + b, 0)}`)
+
 allPackets.push(splitPacket('[[2]]'))
 allPackets.push(splitPacket('[[6]]'))
 allPackets.sort(isSorted)
 
 const packetsAsStrings = allPackets.map((packet) => packet.join(',').replace(/[,]?([\[\]])[,]?/g, '$1'))
-
 const firstMarkerPosition = packetsAsStrings.indexOf('[[2]]') + 1
 const secondMarkerPosition = packetsAsStrings.indexOf('[[6]]') + 1
-console.log(`Answer for part 2 should be 26670`)
+
 console.log(`Answer for part 2: ${firstMarkerPosition * secondMarkerPosition}`)
