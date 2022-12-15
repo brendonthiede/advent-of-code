b0ca24b7fbe1b8f5172af0d551a480c9c18c5ce1
Brendon Thiede <brendonthiede@filevine.com>
2022-12-15 16:16:56 -0500

    2022, day 15 solution

--- 2022/day15/solution.js
+++ 2022/day15/solution.js
@@ -27,92 +27,27 @@ function getInputs(type) {
     return { pairs, sensors, beacons, coverageRanges }
 }
 
-function findHoles(inputs, row) {
-    const beacons = inputs.beacons
-    return inputs.pairs.reduce((holes, [sx, sy, bx, by]) => {
-        const manhattanDistance = Math.abs(sx - bx) + Math.abs(sy - by)
-        if (row >= sy - manhattanDistance && row <= sy + manhattanDistance) {
-            const xReach = Math.abs(manhattanDistance - Math.abs(sy - row))
-            for (let x = sx - xReach; x <= sx + xReach; x++) {
-                if (!beacons.has(`${x},${row}`) && !holes.has(`${x},${row}`)) holes.add(`${x},${row}`)
+function countHoles(inputs, row) {
+    const beaconsInRow = inputs.pairs.reduce((unique, [sx, sy, bx, by]) => { return by === row ? unique.add(bx) : unique }, new Set()).size
+    const holes = inputs.coverageRanges[row].reduce((count, range) => count + range[1] - range[0] + 1, 0)
+    return holes - beaconsInRow
+}
+
+function findLocation(inputs, limit) {
+    for (let row = 0; row <= limit; row++) {
+        const rangesToConsider = inputs.coverageRanges[row]
+        for (let index = 0; index < rangesToConsider.length; index++) {
+            const range = rangesToConsider[index];
+            if (range[0] <= 0 && range[1] < limit) {
+                return { x: range[1] + 1, y: row }
             }
         }
-        return holes
-    }, new Set())
+    }
 }
-
-function runSample() {
-    const samples = getInputs('sample')
-
-    for (let row = -2; row <= 22; row++) {
-        const holes = findHoles(samples, row)
-        let output = ''
-        for (let col = -4; col <= 26; col++) {
-            const pos = `${col},${row}`
-            if (samples.sensors.has(pos)) output += 'S'
-            else if (samples.beacons.has(pos)) output += 'B'
-            else if (holes.has(pos)) output += '#'
-            else output += '.'
-        }
-        console.log(output)
-    }
-    const part1 = findHoles(samples, 10).size
-    if (part1 !== 26) throw new Error(`Expected 26, got ${part1}`)
-    console.log(`Answer for part 1 with sample input is: ${part1}`)
-
-    for (let row = 0; row <= 20; row++) {
-        if (samples.coverageRanges[row]) {
-            console.log(`Row ${row}: ` + samples.coverageRanges[row].reduce((output, range) => output + `[${range[0]},${range[1]}]`, ''))
-        }
-    }
-
-    for (let row = 0; row <= 20; row++) {
-        const holes = findHoles(samples, row)
-        let rowLabel = `${' ' + row}`
-        rowLabel = rowLabel.substring(rowLabel.length - 2)
-        let output = `Row ${rowLabel}: `
-        for (let col = 0; col <= 20; col++) {
-            const pos = `${col},${row}`
-            if (samples.sensors.has(pos)) output += 'S'
-            else if (samples.beacons.has(pos)) output += 'B'
-            else if (holes.has(pos)) output += '#'
-            else output += '.'
-        }
-        console.log(output)
-    }
-
-    const limit = 20
-    let location = null
-    for (let row = 0; row <= limit; row++) {
-        const rangesToConsider = samples.coverageRanges[row]
-        if (rangesToConsider) {
-            rangesToConsider.forEach(range => {
-                if (range[0] <= 0 && range[1] < limit) {
-                    location = { x: range[1] + 1, y: row }
-                }
-            });
-        }
-        if (location) break
-    }
-    console.log(`Answer for part 2 with sample input is: ${location.x * 4000000 + location.y}`)
-}
-
-runSample(); return
 
 const inputs = getInputs('input')
-console.log(`Answer for part 1: ${findHoles(inputs, 2000000).size}`)
 
-const limit = 4000000
-let location = null
-for (let row = 0; row <= limit; row++) {
-    const rangesToConsider = inputs.coverageRanges[row]
-    if (rangesToConsider) {
-        rangesToConsider.forEach(range => {
-            if (range[0] <= 0 && range[1] < limit) {
-                location = { x: range[1] + 1, y: row }
-            }
-        });
-    }
-    if (location) break
-}
+console.log(`Answer for part 1: ${countHoles(inputs, 2000000)}`)
+
+const location = findLocation(inputs, 4000000)
 console.log(`Answer for part 2: ${location.x * 4000000 + location.y}`)
