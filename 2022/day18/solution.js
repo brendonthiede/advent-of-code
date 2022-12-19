a327d2622f1e6a146e6566750a871ad65bfe2890
Brendon Thiede <brendon.thiede@gmail.com>
2022-12-18 23:35:58 -0500

    2022, day 18 solution part 1

--- 2022/day18/solution.js
+++ 2022/day18/solution.js
@@ -1,14 +1,16 @@
+function getUnblockedSurfaceArea(cubes) {
+    return cubes.reduce((surfaceArea, row) => {
+        const cube = row.split(',').map(Number)
+        for (direction of [[1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, -1, 0], [0, 0, 1], [0, 0, -1]]) {
+            const neighbor = `${cube[0] + direction[0]},${cube[1] + direction[1]},${cube[2] + direction[2]}`
+            if (!cubes.includes(neighbor)) surfaceArea++
+        }
+        return surfaceArea
+    }, 0)
+}
+
 const fs = require('fs')
-const cubes = new Set(fs.readFileSync(`${__dirname}/input.txt`, 'utf8')
-    .split(/\r?\n/))
+const cubes = fs.readFileSync(`${__dirname}/input.txt`, 'utf8')
+    .split(/\r?\n/)
 
-const answerPart1 = Array.from(cubes).reduce((surfaceArea, row) => {
-    const cube = row.split(',').map(Number)
-    for (direction of [[1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, -1, 0], [0, 0, 1], [0, 0, -1]]) {
-        const neighbor = `${cube[0] + direction[0]},${cube[1] + direction[1]},${cube[2] + direction[2]}`
-        if (!cubes.has(neighbor)) surfaceArea++
-    }
-    return surfaceArea
-}, 0)
-
-console.log(`Answer part 1: ${answerPart1}`)
+console.log(`Answer part 1: ${getUnblockedSurfaceArea(cubes)}`)
