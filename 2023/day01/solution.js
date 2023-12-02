8358a0573ddd206a3c795dfa41b95bb1344c2e03
Brendon Thiede <brendon.thiede@gmail.com>
2023-12-01 21:08:10 -0500

    Day 1, 2023

--- 2023/day01/solution.js
+++ 2023/day01/solution.js
@@ -6,21 +6,19 @@ const part1 = inputs.map(line => line.replace(/\D/g, ''))
     .map(line => line.slice(0, 1) + line.slice(-1))
     .map(line => parseInt(line))
     .reduce((a, b) => a + b, 0);
-    
-    
-    function partTwoConversion(line) {
-        const numberWords = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-        // can't just replace the word, because it may be part of another word, but duplicating the word for now will work
-        numberWords.forEach((word, index) => {
-            const regex = new RegExp(word, 'gi');
-            line = line.replace(regex, word + index + word);
-        })
-        
-        return line;
-    }
-    
-    
-    const part2 = inputs
+
+function partTwoConversion(line) {
+    const numberWords = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    // can't just replace the word, because it may be part of another word, but duplicating the word for now will work
+    numberWords.forEach((word, index) => {
+        const regex = new RegExp(word, 'gi');
+        line = line.replace(regex, word + index + word);
+    })
+
+    return line;
+}
+
+const part2 = inputs
     .map(line => partTwoConversion(line))
     .map(line => line.replace(/\D/g, ''))
     .map(line => line.slice(0, 1) + line.slice(-1))
@@ -29,5 +27,3 @@ const part1 = inputs.map(line => line.replace(/\D/g, ''))
 
 console.log("Answer for part 1: " + part1);
 console.log("Answer for part 2: " + part2);
-
-// < 55445
