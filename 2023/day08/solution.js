9bc9f791b460e7e6ef849b6a3ba63b5ef251c9a7
Brendon Thiede <brendon.thiede@gmail.com>
2023-12-08 07:48:46 -0500

    Day 8, 2023

--- 2023/day08/solution.js
+++ 2023/day08/solution.js
@@ -34,41 +34,27 @@ function aaaToZZZ(directions, network) {
   return steps;
 }
 
-function aToZCycles(directions, network) {
+// manual inspection showed that my inputs all had exactly 1 cycle that would come back to itself in the same number of steps as originally taken to get to it
+// this allowed for some cleanup, but some input will definitely break this
+function aToZCycleLengths(directions, network) {
   const currentNodes = network.filter(node => /.*A$/.test(node.node)).map(node => node.node);
-  
-  // need to identify the cycles
-  const cycles = currentNodes.map(node => {
-    const cycles = [];
-    const startingNode = node;
-    let nextDirection = 1;
-    let { left, right } = network.find(({ node }) => node === startingNode);
-    let currentNode = directions[0] === 'L' ? left : right;
-    let steps = 1;
-    let totalSteps = 1;
-    while (currentNode !== startingNode) {
+  const cycleLengths = currentNodes.map(node => {
+    let nextDirection = 0;
+    let steps = 0;
+    let currentNode = node;
+    while (true) {
       let { left, right } = network.find(({ node }) => node === currentNode);
       currentNode = directions[nextDirection] === 'L' ? left : right;
       nextDirection = (nextDirection + 1) % directions.length;
       steps++;
-      totalSteps++;
       if (/.*Z$/.test(currentNode)) {
-        // have we already seen this cycle and was it as a multiple of the number of directions?
-        const cycle = cycles.find(cycle => cycle.node === currentNode && cycle.steps === steps);
-        if (cycle && totalSteps % directions.length === 0) {
-          break;
-        }
-        cycles.push({
-          node: currentNode,
-          steps
-        });
-        steps = 0;
+        break;
       }
     }
-    return cycles;
+    return steps;
   });
 
-  return cycles;
+  return cycleLengths;
 }
 
 function gcd(a, b) {
@@ -80,11 +66,9 @@ function lcm(a, b) {
   return (a * b) / gcd(a, b);
 }
 
-function lowestCommonMultiple(cycles) {
+function lowestCommonMultiple(cycleLengths) {
   // find the lowest common multiple of the cycles
-  // manual inspection showed that my inputs all had exactly 1 cycle
-  const cycleSteps = cycles.map(cycle => cycle[0].steps);
-  const lowestCommonMultiple = cycleSteps.reduce((a, b) => {
+  const lowestCommonMultiple = cycleLengths.reduce((a, b) => {
     return lcm(a, b);
   });
   return lowestCommonMultiple;
@@ -98,12 +82,14 @@ if (inputType === 'sample' || inputType === 'input') {
 }
 
 if (inputType === 'sample2' || inputType === 'input') {
-  part2 = lowestCommonMultiple(aToZCycles(directions, network));
+  part2 = lowestCommonMultiple(aToZCycleLengths(directions, network));
 }
 
 if (inputType === 'sample') {
   console.log(`Answer for part 1: ${part1} (should be 6)`);
+  console.log('Answer for part 2 cannot be calculated from sample');
 } else if (inputType === 'sample2') {
+  console.log('Answer for part 1 cannot be calculated from sample2');
   console.log(`Answer for part 2: ${part2} (should be 6)`);
 } else {
   console.log(`Answer for part 1: ${part1} (should be 21251)`);
