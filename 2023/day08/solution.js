88c32117711e0a0324149d6df64578a8cf21ddde
Brendon Thiede <brendonthiede@filevine.com>
2023-12-08 07:33:33 -0500

    Day 8, 2023

--- 2023/day08/solution.js
+++ 2023/day08/solution.js
@@ -8,7 +8,7 @@ if (process.argv.length > 2) {
 }
 
 let DEBUG = false;
-if (inputType === 'sample' || process.argv.length > 3) {
+if (/sample.*/.test(inputType) || process.argv.length > 3) {
   DEBUG = true;
 }
 
@@ -27,18 +27,85 @@ function aaaToZZZ(directions, network) {
   let steps = 0;
   while (currentNode !== 'ZZZ') {
     const { left, right } = network.find(({ node }) => node === currentNode);
-    const nextNode = directions[nextDirection] === 'L' ? left : right;
-    currentNode = nextNode;
+    currentNode = directions[nextDirection] === 'L' ? left : right;
     nextDirection = (nextDirection + 1) % directions.length;
     steps++;
   }
   return steps;
 }
 
-part1 = aaaToZZZ(directions, network);
+function aToZCycles(directions, network) {
+  const currentNodes = network.filter(node => /.*A$/.test(node.node)).map(node => node.node);
+  
+  // need to identify the cycles
+  const cycles = currentNodes.map(node => {
+    const cycles = [];
+    const startingNode = node;
+    let nextDirection = 1;
+    let { left, right } = network.find(({ node }) => node === startingNode);
+    let currentNode = directions[0] === 'L' ? left : right;
+    let steps = 1;
+    let totalSteps = 1;
+    while (currentNode !== startingNode) {
+      let { left, right } = network.find(({ node }) => node === currentNode);
+      currentNode = directions[nextDirection] === 'L' ? left : right;
+      nextDirection = (nextDirection + 1) % directions.length;
+      steps++;
+      totalSteps++;
+      if (/.*Z$/.test(currentNode)) {
+        // have we already seen this cycle and was it as a multiple of the number of directions?
+        const cycle = cycles.find(cycle => cycle.node === currentNode && cycle.steps === steps);
+        if (cycle && totalSteps % directions.length === 0) {
+          break;
+        }
+        cycles.push({
+          node: currentNode,
+          steps
+        });
+        steps = 0;
+      }
+    }
+    return cycles;
+  });
+
+  return cycles;
+}
+
+function gcd(a, b) {
+  if (b === 0) return a;
+  return gcd(b, a % b);
+}
+
+function lcm(a, b) {
+  return (a * b) / gcd(a, b);
+}
+
+function lowestCommonMultiple(cycles) {
+  // find the lowest common multiple of the cycles
+  // manual inspection showed that my inputs all had exactly 1 cycle
+  const cycleSteps = cycles.map(cycle => cycle[0].steps);
+  const lowestCommonMultiple = cycleSteps.reduce((a, b) => {
+    return lcm(a, b);
+  });
+  return lowestCommonMultiple;
+}
+
+let part1 = 0;
+let part2 = 0;
+
+if (inputType === 'sample' || inputType === 'input') {
+  part1 = aaaToZZZ(directions, network);
+}
+
+if (inputType === 'sample2' || inputType === 'input') {
+  part2 = lowestCommonMultiple(aToZCycles(directions, network));
+}
 
 if (inputType === 'sample') {
   console.log(`Answer for part 1: ${part1} (should be 6)`);
+} else if (inputType === 'sample2') {
+  console.log(`Answer for part 2: ${part2} (should be 6)`);
 } else {
   console.log(`Answer for part 1: ${part1} (should be 21251)`);
+  console.log(`Answer for part 2: ${part2} (should be 11678319315857)`);
 }
