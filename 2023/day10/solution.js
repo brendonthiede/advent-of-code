c36aa2833b0442051592e0801716e380409ff1c4
Brendon Thiede <brendon.thiede@gmail.com>
2023-12-10 22:29:01 -0500

    Day 10, 2023

--- 2023/day10/solution.js
+++ 2023/day10/solution.js
@@ -138,23 +138,97 @@ while (true) {
     console.log(currentPos, currentPipe, facing);
   }
   const next = getNext(currentPos, currentPipe, facing);
-    currentPos = next.nextPos;
-    currentPipe = next.nextPipe;
-    facing = next.nextFacing;
-    loop.push(currentPos);
-    if (currentPipe === 'S') {
-      break;
-    }
+  currentPos = next.nextPos;
+  currentPipe = next.nextPipe;
+  facing = next.nextFacing;
+  loop.push(currentPos);
+  if (currentPipe === 'S') {
+    break;
+  }
 }
 
+// double spacing everything, to leave gaps between pipes without connections
+
+// mark everything as a 1 to start with
+const plane = Array(input.length * 2).fill(0).map(x => Array(input[0].length * 2).fill(1));
+
+// mark the pipes and connections between them as X's
+for (let i = 1; i < loop.length; i++) {
+  const current = loop[i].map(x => x * 2);
+  const prev = loop[i - 1].map(x => x * 2);
+  const inBetween = [(current[0] + prev[0]) / 2, (current[1] + prev[1]) / 2];
+  plane[current[1]][current[0]] = 'X';
+  plane[inBetween[1]][inBetween[0]] = 'X';
+}
+
+// Starting at the edges, mark everything touching the border as 0 and fill that in until pipes are hit
+let found = true;
+while (found) {
+  found = false;
+  for (let y = 0; y < plane.length; y++) {
+    for (let x = 0; x < plane[0].length; x++) {
+      if (plane[y][x] === 'X' || plane[y][x] === 0) {
+        continue;
+      }
+      if (y === 0 || y === plane.length - 1 || x === 0 || x === plane[0].length - 1) {
+        found = true;
+        plane[y][x] = 0;
+        continue;
+      }
+      for (let i = 0; i < directions.length; i++) {
+        const direction = directions[i];
+        const xOffset = direction === 'e' ? 1 : direction === 'w' ? -1 : 0;
+        const yOffset = direction === 's' ? 1 : direction === 'n' ? -1 : 0;
+
+        const neighbor = [x + xOffset, y + yOffset];
+        if (neighbor[0] < 0 || neighbor[0] >= plane[0].length || neighbor[1] < 0 || neighbor[1] >= plane.length) {
+          continue;
+        }
+        if (plane[neighbor[1]][neighbor[0]] === 0) {
+          found = true;
+          plane[y][x] = 0;
+        }
+      }
+    }
+  }
+}
+
+const shrunkPlane = [];
+for (let y = 0; y < plane.length; y += 2) {
+  const row = [];
+  for (let x = 0; x < plane[0].length; x += 2) {
+    row.push(plane[y][x]);
+  }
+  shrunkPlane.push(row);
+}
+
+if (DEBUG) {
+  for (let y = 0; y < plane.length; y++) {
+    let line = '';
+    for (let x = 0; x < plane[0].length; x++) {
+      if (plane[y][x] === 0) {
+        line += ' ';
+      } else {
+        line += plane[y][x];
+      }
+    }
+    console.log(line);
+  }
+}
 
 const part1 = (loop.length - 1) / 2;
-const part2 = 0;
+const part2 = shrunkPlane.reduce((acc, row) => acc + row.reduce((acc, cell) => acc + (cell === 1 ? 1 : 0), 0), 0);
 
-if (/sample.*/.test(inputType)) {
+if (inputType === 'sample') {
   console.log(`Answer for part 1: ${part1} (should be 8)`);
   assert(part1 === 8);
+} else if (inputType === 'sample2') {
+  console.log(`Answer for part 1: ${part1} (should be 70)`);
+  assert(part1 === 70);
+  console.log(`Answer for part 2: ${part2} (should be 8)`);
+  assert(part2 === 8);
 } else {
   console.log(`Answer for part 1: ${part1} (should be 7097)`);
   assert(part1 === 7097);
+  console.log(`Answer for part 2: ${part2} (should be 355)`);
 }
