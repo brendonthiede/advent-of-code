fd96db11eedbd432e3aae19206523d0d38c578c2
Brendon Thiede <brendonthiede@filevine.com>
2023-12-14 21:40:00 -0500

    Day 14, 2023

--- 2023/day14/solution.js
+++ 2023/day14/solution.js
@@ -17,58 +17,148 @@ if (/sample.*/.test(inputType) || process.argv.length > 3) {
 const input = fs.readFileSync(`${__dirname}/${inputType}.txt`, 'utf8')
   .split(/\r?\n/);
 
-function rotate(input, direction) {
+function tilt(input, direction) {
   const output = [];
-  if (direction === 'counterclockwise') {
-
-    for (let i = input[0].length - 1; i >= 0; i--) {
-      let column = '';
-      for (let j = 0; j < input.length; j++) {
-        column += input[j].substr(i, 1);
+  switch (direction) {
+    case 'north':
+      // gather each column into a line, so top is left, and collapse
+      for (let charIndex = input[0].length - 1; charIndex >= 0; charIndex--) {
+        let column = '';
+        for (let rowIndex = 0; rowIndex < input.length; rowIndex++) {
+          column += input[rowIndex].substr(charIndex, 1);
+        }
+        output.push(column);
       }
-      output.push(column);
-    }
-  } else {
-    for (let i = 0; i < input[0].length; i++) {
-      let column = '';
-      for (let j = input.length - 1; j >= 0; j--) {
-        column += input[j].substr(i, 1);
+      collapse(output);
+      // put the collapsed columns back into rows
+      for (let charIndex = 0; charIndex < output[0].length; charIndex++) {
+        let column = '';
+        for (let rowIndex = output.length - 1; rowIndex >= 0; rowIndex--) {
+          column += output[rowIndex].substr(charIndex, 1);
+        }
+        input[charIndex] = column;
       }
-      output.push(column);
-    }
+      break;
+    case 'south':
+      // gather each column into a line, so bottom is left, and collapse
+      for (let charIndex = 0; charIndex < input[0].length; charIndex++) {
+        let column = '';
+        for (let rowIndex = input.length - 1; rowIndex >= 0; rowIndex--) {
+          column += input[rowIndex].substr(charIndex, 1);
+        }
+        output.push(column);
+      }
+      collapse(output);
+      // put the collapsed columns back into rows
+      for (let charIndex = output[0].length - 1; charIndex >= 0; charIndex--) {
+        let column = '';
+        for (let rowIndex = 0; rowIndex < output.length; rowIndex++) {
+          column += output[rowIndex].substr(charIndex, 1);
+        }
+        input[output.length - charIndex - 1] = column;
+      }
+      break;
+    case 'east':
+      // gather each row into a line, so right is left, and collapse
+      input.forEach((line) => {
+        output.push(line.split('').reverse().join(''));
+      });
+      collapse(output);
+      // put the collapsed rows back into columns
+      output.forEach((line, index) => {
+        input[index] = line.split('').reverse().join('');
+      });
+      break;
+    case 'west':
+      collapse(input);
+      break;
   }
-  return output;
 }
 
-// rotate the input 90 degrees counter-clockwise
-const northerly = rotate(input, 'counterclockwise');
+function collapse(input) {
+  // collapse the input to the left
+  input.forEach((line, index) => {
+    let newLine = line.replace(/([.]+)O/g, 'O$1');
+    while (newLine !== line) {
+      line = newLine;
+      newLine = line.replace(/([.]+)O/g, 'O$1');
+    }
 
-// collapse the input to the left/north
-northerly.forEach((line, index) => {
-  let newLine = line.replace(/([.]+)O/g, 'O$1');
-  while (newLine !== line) {
-    line = newLine;
-    newLine = line.replace(/([.]+)O/g, 'O$1');
+    input[index] = newLine;
+  });
+}
+
+function getWeight(input) {
+  return input.reduce((sum, line, index) => {
+    const regex = /O/g;
+    const stones = regex.test(line) ? line.match(/[O]/g).length : 0;
+    const weight = input.length - index;
+    return sum + (stones * weight);
+  }, 0);
+}
+
+function printWithWeight(input) {
+  input.forEach((line, index) => {
+    let weight = `   ${input.length - index}`
+    weight.substring(weight.length - 3);
+    console.log(`${line} ${weight}`);
+  });
+  console.log('\n\n');
+}
+
+const part1Input = input.slice(0);
+
+if (DEBUG) {
+  console.log('Before:');
+  printWithWeight(part1Input);
+}
+
+tilt(part1Input, 'north');
+
+if (DEBUG) {
+  console.log('After:');
+  printWithWeight(part1Input);
+}
+
+const part1 = getWeight(part1Input);
+
+const directions = ['north', 'west', 'south', 'east'];
+let part2Input = input.slice(0);
+if (DEBUG) {
+  console.log('Before:');
+  printWithWeight(part2Input);
+}
+
+const history = [];
+
+const numberOfCycles = 1000000000;
+const totalTilts = numberOfCycles * directions.length;
+let final = [];
+for (let i = 0; i < totalTilts; i++) {
+  let direction = directions[i % directions.length];
+  tilt(part2Input, direction);
+  let current = `${direction}|${part2Input.join('|')}`;
+  let cycleStart = history.indexOf(current);
+  history.push(current);
+  if (cycleStart >= 0) {
+    const cycleLength = i - cycleStart;
+    const remainingTilts = totalTilts - i;
+    const finalPosition = cycleStart + (remainingTilts % cycleLength) - 1;
+    final = history[finalPosition].split('|');
+    break;
   }
+}
 
-  northerly[index] = newLine;
-});
-
-const upsideDown = rotate(northerly, 'counterclockwise');
-
-let part1 = upsideDown.reduce((sum, line, index) => {
-  const regex = /O/g;
-  const stones = regex.test(line) ? line.match(/[O]/g).length : 0;
-  const weight = index + 1;
-  return sum + (stones * weight);
-}, 0);
-
-let part2 = 0;
+const part2 = getWeight(final);
 
 if (/sample.*/.test(inputType)) {
   console.log(`Answer for part 1: ${part1} (should be 136)`);
   assert(part1 === 136);
+  console.log(`Answer for part 2: ${part2} (should be 64)`);
+  assert(part2 === 64);
 } else {
   console.log(`Answer for part 1: ${part1} (should be 105784)`);
   assert(part1 === 105784);
+  console.log(`Answer for part 2: ${part2} (should be 91286)`);
+  assert(part2 === 91286);
 }
