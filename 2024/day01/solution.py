fd17d8802a5c19646c5a0cf207e9d44a73037a0a
Brendon Thiede <brendon.thiede@gmail.com>
2024-12-01 18:41:42 -0500

    day01-2024

--- 2024/day01/solution.py
+++ 2024/day01/solution.py
@@ -14,16 +14,8 @@ for line in inputs.splitlines():
     first_column.append(a)
     second_column.append(b)
 
-# Sort the columns
-first_column.sort()
-second_column.sort()
-
-sum_of_differences = sum(abs(a - b) for a, b in zip(first_column, second_column))
+sum_of_differences = sum(abs(a - b) for a, b in zip(sorted(first_column), sorted(second_column)))
 print("Answer for part 1:", sum_of_differences)
 
-frequency = [0] * len(first_column)
-for i, value in enumerate(first_column):
-    frequency[i] = second_column.count(value)
-
-frequency_weighted_sum = sum(first_column[i] * frequency[i] for i in range(len(first_column)))
+frequency_weighted_sum = sum(i * second_column.count(i) for i in first_column)
 print("Answer for part 2:", frequency_weighted_sum)
