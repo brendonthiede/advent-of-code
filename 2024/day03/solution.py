c6c83dc550b0c5109ebe0197dcc513b96b238396
Brendon Thiede <brendon.thiede@gmail.com>
2024-12-03 00:35:17 -0500

    day03-2024

--- 2024/day03/solution.py
+++ 2024/day03/solution.py
@@ -7,19 +7,17 @@ with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r") as file:
     inputs = file.read()
 
 def multiply_some_numbers(input_text, dont_value):
-    pattern = re.compile(r'(mul\((\d+),(\d+)\)|do\(\)|don\'t\(\))')
+    pattern = re.compile(r'mul\((\d+),(\d+)\)|do\(\)|don\'t\(\)')
     total = 0
     do_operation = True
-    for line in input_text.splitlines():
-        for match in pattern.finditer(line):
-            operator = match.group(1)
-            if operator == "do()":
-                do_operation = True
-            elif operator == "don't()":
-                do_operation = dont_value
-            elif do_operation and operator.startswith("mul("):
-                first, second = map(int, match.groups()[1:])
-                total += first * second
+    for match in pattern.finditer(input_text):
+        if match.group() == "do()":
+            do_operation = True
+        elif match.group() == "don't()":
+            do_operation = dont_value
+        elif do_operation:
+            first, second = map(int, match.groups()[:2])
+            total += first * second
     return total
 
 part_one_value = multiply_some_numbers(inputs, True)
