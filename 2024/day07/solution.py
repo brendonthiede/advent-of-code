35f265a7873a131e00c3b6f48e7a659a5069aba2
Brendon Thiede <brendon.thiede@gmail.com>
2024-12-07 23:34:27 -0500

    day07-2024

--- 2024/day07/solution.py
+++ 2024/day07/solution.py
@@ -18,23 +18,26 @@ for line in lines:
 
 def solver(data, operator_options):
     grand_total = 0
-    def evaluate_expression(values, operators):
-        result = values[0]
-        for i in range(1, len(values)):
-            if operators[i-1] == '+':
-                result += values[i]
-            elif operators[i-1] == '*':
-                result *= values[i]
-            elif operators[i-1] == '||':
-                result = int(f'{result}{values[i]}')
-        return result
 
     def try_operators(total, values, operator_options):
-        for operators in product(operator_options, repeat=len(values) - 1):
-            if evaluate_expression(values, operators) == total:
-                return True
-        return False
-
+        def iterative_try(values, operators, current_result, index):
+            if index == len(values) - 1:
+              return current_result == total
+            for operator in operators:
+                if operator == '+':
+                    new_result = current_result + values[index + 1]
+                elif operator == '*':
+                    new_result = current_result * values[index + 1]
+                elif operator == '||':
+                    new_result = int(f'{current_result}{values[index + 1]}')
+                # all operators increase the value, so we can stop traversing this branch if the result is already greater than the desired total
+                if new_result > total:
+                    continue
+                if iterative_try(values, operators, new_result, index + 1):
+                    return True
+            return False
+        return iterative_try(values, operator_options, values[0], 0)
+    
     for total, values in data:
         if try_operators(total, values, operator_options):
             grand_total += total
