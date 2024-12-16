65863d67c0484a3160603374f20bfe888dd663fe
Brendon Thiede <brendon.thiede@gmail.com>
2024-12-16 00:38:06 -0500

    solution timer 2024

--- 2024/timed_run.sh
+++ 2024/timed_run.sh
@@ -1,17 +1,54 @@
 #!/usr/bin/env bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+PARENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." >/dev/null 2>&1 && pwd)"
 
-find "${DIR}" -name "*.py" -type f -exec chmod +x {} \;
+echo $PARENT_DIR
 
-SOLUTION_DAYS="$(find "${DIR}" -maxdepth 1 -name "day*" -type d)"
+usage() {
+    local -r _exitCode="${1}"
+    local -r _errorMessage="${2}"
+    if [[ -n "${_errorMessage}" ]]; then
+        printf "[ERROR] %s\n\n" "${_errorMessage}"
+    fi
+    echo " Adds the provided users to the aws-auth configmap in the active EKS cluster."
+    echo >&2 " Usage: $0 [-c context] -g group1 -u username1 [username2 ...] [-g group2 -u username3 [username4 ...]] [-d] [-y] [-h]"
+    echo >&2 -e "\t-v | --verbose [Optional] If this flag is set output will be more verbose, including each solution output."
+    echo >&2 -e "\t-h | --help    Print this message."
+    echo >&2 ""
+    echo >&2 " Example:"
+    echo >&2 -e "\t\t$0 -v"
+    exit $1
+}
 
-SOLUTION_DAYS=$(echo "${SOLUTION_DAYS}" | tr ' ' '\n' | sort -V)
+VERBOSE="false"
+while [ -n "$1" ]; do
+    case "$1" in
+    -v | --verbose)
+        VERBOSE="true"
+        ;;
+    -h | --help)
+        usage 0
+        ;;
+    *)
+        usage 1 "Invalid parameter '$1'"
+        ;;
+    esac
+    shift
+done
 
-for DAY in ${SOLUTION_DAYS}; do
-    echo -e "\n${DAY}"
+SOLUTION_DAYS="$(find "${DIR}" -maxdepth 1 -name "day*" -type d | tr ' ' '\n' | sort -V)"
+
+for DAY_PATH in ${SOLUTION_DAYS}; do
     START_TIME=$(date +%s%3N)
-    python3 "${DAY}/solution.py"
+    SOLUTION="$(python3 "${DAY_PATH}/solution.py")"
     END_TIME=$(date +%s%3N)
     ELAPSED_TIME=$((END_TIME - START_TIME))
-    echo "Elapsed time: ${ELAPSED_TIME} ms"
+    SHORT_DAY_PATH="${DAY_PATH#${PARENT_DIR}/}"
+    if [[ "${VERBOSE}" == "true" ]]; then
+        echo -e "\n${DAY_PATH}"
+        echo "${SOLUTION}"
+        echo "Elapsed time: ${ELAPSED_TIME} ms"
+    else
+        echo "${SHORT_DAY_PATH}: ${ELAPSED_TIME} ms"
+    fi
 done
