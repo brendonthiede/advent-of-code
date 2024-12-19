098909691661331d670292027c28dd20d27bc6d2
Brendon Thiede <brendon.thiede@gmail.com>
2024-12-19 00:23:11 -0500

    day19-2024

--- 2024/day19/solution.py
+++ 2024/day19/solution.py
@@ -9,51 +9,60 @@ def parse_input(filename):
     designs = [d.strip() for d in designs.split('\n')]
     return patterns, designs
 
-def can_make_design(design, patterns):
+def can_make_design(design, patterns, pattern_lengths):
     n = len(design)
-    # dp[i] represents if we can make the substring design[0:i]
+    
     dp = [False] * (n + 1)
     dp[0] = True  # Empty string is always possible
     
-    # For each position in the design
     for i in range(n):
         if not dp[i]:
             continue
-        # Try each pattern at current position
-        for pattern in patterns:
-            if design[i:].startswith(pattern):
-                dp[i + len(pattern)] = True
-    
+        # Only check patterns of lengths that could possibly fit
+        for length in pattern_lengths:
+            if i + length <= n:
+                substr = design[i:i + length]
+                if substr in patterns:
+                    dp[i + length] = True
     return dp[n]
 
-def count_ways_to_make_design(design, patterns):
+def count_ways_to_make_design(design, pattern_dict):
     n = len(design)
-    # dp[i] represents the number of ways to make substring design[0:i]
+    
     dp = [0] * (n + 1)
     dp[0] = 1  # Empty string can be made in one way
     
-    # For each position in the design
     for i in range(n):
         if dp[i] == 0:
             continue
-        # Try each pattern at current position
-        for pattern in patterns:
-            if design[i:].startswith(pattern):
-                dp[i + len(pattern)] += dp[i]
-    
+        # Only check patterns of the right length
+        for length, patterns_of_length in pattern_dict.items():
+            if i + length <= n:
+                substr = design[i:i + length]
+                if substr in patterns_of_length:
+                    dp[i + length] += dp[i]
     return dp[n]
 
 def part_one(patterns, designs):
+    # Precompute pattern lengths
+    pattern_lengths = {len(p) for p in patterns}
+    patterns = set(patterns)
+    
     possible_count = 0
     for design in designs:
-        if can_make_design(design, patterns):
+        if can_make_design(design, patterns, pattern_lengths):
             possible_count += 1
     return possible_count
 
 def part_two(patterns, designs):
+    # Group patterns by length for faster lookup
+    pattern_dict = {}
+    for p in patterns:
+        pattern_dict.setdefault(len(p), set()).add(p)
+    
     total_ways = 0
     for design in designs:
-        ways = count_ways_to_make_design(design, patterns)
+        ways = count_ways_to_make_design(design, pattern_dict)
         total_ways += ways
     return total_ways
 
